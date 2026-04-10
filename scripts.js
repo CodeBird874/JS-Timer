1a9c5648f9105787c68fbff8bda6068a228adaab
CodeBird874 <freej447@protonmail.com>
2026-04-10 12:47:15 -0500

    added hours, more code will have to be formatted and fixed for this update.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 
 let timerLabel = document.getElementById("timerLabel");
 let alarmSound = document.getElementById("alarmsound");
+let hours;
 let minutes;
 let seconds;
 let invalidTime = false;
 alarmSound.loop = true;
 // input from prompt is tprompt is turned from a string to an int.
 function gatherInput() {
+
+
+  let enterHours = prompt("Please enter hours: (must be a positive number below 24)", "1");
 
   let enterMinutes = prompt("Please enter minutes: (must be a positive number below 60)", "10");
 
@@ -16,6 +20,7 @@ function gatherInput() {
 
 
 
+  hours = parseInt(enterHours);
   minutes = parseInt(enterMinutes);
   seconds = parseInt(enterSeconds);
 
@@ -24,7 +29,7 @@ function gatherInput() {
 
 
 // checks input and sets a bool off that prevents the clock from doing anything when it is created 
-// if input is more than 60 or not entered.
+// if input is more than specified time or not entered.
 function checkInputValidity() {
   if (minutes > 59 || minutes < 0) {
 
@@ -35,9 +40,13 @@ function checkInputValidity() {
   if (seconds > 59 || seconds < 0) {
 
     invalidTime = true;
+
+  }
+  if (hours > 23 || hours < 0) {
+    invalidTime = true;
   }
 
-  if (isNaN(minutes) || isNaN(seconds)) {
+  if (isNaN(minutes) || isNaN(seconds) || isNaN(hours)) {
     invalidTime = true;
   }
   if (invalidTime) {
@@ -52,17 +61,17 @@ function checkInputValidity() {
 function formatTimer() {
   if (minutes <= 9) {
 
-    timerLabel.textContent = "0" + minutes + ":" + seconds;
+    timerLabel.textContent = hours + ":" + "0" + minutes + ":" + seconds;
   }
 
   if (seconds <= 9) {
-    timerLabel.textContent = minutes + ":" + "0" + seconds;
+    timerLabel.textContent = hours + ":" + minutes + ":" + "0" + seconds;
   }
   if (minutes <= 9 && seconds <= 9) {
-    timerLabel.textContent = "0" + minutes + ":" + "0" + seconds;
+    timerLabel.textContent = hours + "0" + minutes + ":" + "0" + seconds;
   }
   if (minutes > 9 && seconds > 9) {
-    timerLabel.textContent = minutes + ":" + seconds;
+    timerLabel.textContent = hours + ":" + minutes + ":" + seconds;
   }
   // formats label for incorrect input.
   if (invalidTime) {
@@ -72,6 +81,9 @@ function formatTimer() {
 
 // prevents minutes from hitting negative and removes a minute when 60 seconds has passed.
 function calculateTime() {
+  if (minutes == 0 && hours > 0) {
+    hours--;
+  }
   if (seconds == 0 && minutes > 0) {
     minutes--;
     // sets seconds to a a full minute. 
@@ -93,7 +105,7 @@ function decrementTimer() {
 function stopTimer() {
   // if seconds are gone, and minutes are gone, remove the clock change the text,
   // and notify the user time is up.
-  if (seconds < 0 && minutes == 0) {
+  if (seconds < 0 && minutes == 0 && hours == 0) {
     clearInterval(Clock);
     alarmSound.play();
     timerLabel.textContent = "Time is up!";
